91dc8acb62c5a3340494de4fee2105717fc048dc
gordislava <gordislava@gmail.com>
2019-04-01 09:56:37 +0300

    trash

--- test-blog/js/common.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-	$(".menu-item--lang").click(function () {
-		$(".menu-item--lang").not($(this)).removeClass('menu-item--lang-active');
-		$(this).toggleClass('menu-item--lang-active');
-	});
-
-});
